7a85b50cf2a156122275a9d419fdb5b6d698d8aa
Claus Aranha <caranha@gmail.com>
2014-04-26 01:34:22 +0900

    Initial Commit

---
+++ android/src/org/castelodelego/ld29/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.castelodelego.ld29.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.castelodelego.ld29.LD29Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD29Game(), config);
+	}
+}
---
+++ desktop/src/org/castelodelego/ld29/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.castelodelego.ld29.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.castelodelego.ld29.LD29Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD29Game(), config);
+	}
+}
